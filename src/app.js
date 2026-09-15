eaa9629b165ddae6f8280dc8058c1c8c99ca748c
Abdulaziz <qodirovabdulazizjon@gmail.com>
2026-09-15 20:44:11 +0500

    add get point to app

--- src/app.js
+++ src/app.js
@@ -17,6 +17,10 @@ import mainRoute from './routes/main.routes.js'
 app.use('/api/v1/auth', authRoute)
 app.use('/api/v1/', mainRoute)
 
+app.get('/', (req, res) => {
+  res.status(200).json({message: 'Hello worlld'})
+})
+
 // 3. Webhook callback (Endi req.body mavjud bo'ladi)
 const webhookPath = `/telegram-webhook/${process.env.BOT_TOKEN}`
 app.use(webhookPath, webhookCallback(bot, "express"))
